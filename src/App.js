b139a06f320d77fc9eb7e6267fd57c6633d7f48f
luke <lukehtravis@gmail.com>
2019-05-05 12:30:28 -0700

    chore: cleanup unused code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {BrowserRouter as Router, Route} from "react-router-dom";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
 import AddSpot from "./Components/AddSpot";
-import LaunchAPI from "./utils/api.js";
 
 class App extends Component {
   render() {
