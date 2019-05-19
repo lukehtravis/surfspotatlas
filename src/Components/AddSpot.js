055d70a7d9efc3f7627d13591052229e0d178da8
luke <lukehtravis@gmail.com>
2019-05-19 09:51:17 -0700

    feat: add form elements to the addspot functionality

--- src/Components/AddSpot.js
+++ src/Components/AddSpot.js
@@ -20,6 +20,8 @@ class AddSpot extends Component {
 
   handleSubmit = (event) => {
     event.preventDefault();
+    const date = new Date();
+    const timestamp = date.getTime();
     this.props.mutate({
         variables: {
           name: this.state.name,
@@ -27,6 +29,8 @@ class AddSpot extends Component {
           description: this.state.description,
           directions: this.state.directions,
           bathymetry: this.state.bathymetry,
+          wavetype: this.state.wavetype,
+          wavelength: this.state.wavelength
         },
     })
   }
@@ -71,30 +75,12 @@ class AddSpot extends Component {
               <option value="reef">Reef</option>
               <option value="sand-reef">Sand And Reef</option>
           </select>
-          <label>
-            Nickname:
-              <input
-                name="nickname"
-                type="text"
-                value={this.state.nickname}
-                onChange={this.handleChange} />
-          </label>
-          <label>
-            Description:
-              <input
-                name="description"
-                type="text"
-                value={this.state.description}
-                onChange={this.handleChange} />
-          </label>
-          <label>
-            Directions:
-              <input
-                name="directions"
-                type="text"
-                value={this.state.directions}
-                onChange={this.handleChange} />
-          </label>
+          <select name="wavetype" value={this.state.wavetype} onChange={this.handleChange} >
+              <option value="beachbreak">Beach Break</option>
+              <option value="reefbreak">Reef Break</option>
+              <option value="pointbreak">Point Break</option>
+          </select>
+          <input type="range" min="1" max="100" value={this.state.wavelength} className="value-slider" id="length-slider" onChange={this.handleChange} />
           <button onClick={this.handleSubmit} />
         </form>
       </div>
@@ -102,14 +88,16 @@ class AddSpot extends Component {
   }
 }
 const MUTATION = gql`
-mutation AddSpot($name: String, $nickname: String, $description: String, $directions: String, $bathymetry: String) {
-  insert_Waves(objects: {name: $name, nickname: $nickname, description: $description, directions: $directions, bathymetry: $bathymetry}) {
+mutation AddSpot($name: String, $nickname: String, $description: String, $directions: String, $bathymetry: String, $wavetype: String, $wavelength: Int) {
+  insert_Waves(objects: {name: $name, nickname: $nickname, description: $description, directions: $directions, bathymetry: $bathymetry, wavetype: $wavetype, wavelength: $wavelength}) {
     returning {
       name,
       nickname,
       description,
       directions,
-      bathymetry
+      bathymetry,
+      wavetype,
+      wavelength
     }
   }
 }
