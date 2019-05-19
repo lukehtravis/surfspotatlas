d971b47253c4a89508ed3421d033b30ce5431256
luke <lukehtravis@gmail.com>
2019-05-19 14:57:22 -0700

    feat: add form elements to addspot

--- src/Components/AddSpot.js
+++ src/Components/AddSpot.js
@@ -30,7 +30,11 @@ class AddSpot extends Component {
           directions: this.state.directions,
           bathymetry: this.state.bathymetry,
           wavetype: this.state.wavetype,
-          wavelength: this.state.wavelength
+          wavelength: this.state.wavelength,
+          wavequality: this.state.wavequality,
+          wavedirection: this.state.wavedirection,
+          wavehollowness: this.state.wavehollowness,
+          wavedanger: this.state.wavedanger
         },
     })
   }
@@ -39,48 +43,74 @@ class AddSpot extends Component {
     return (
       <div>
         <form>
-          <label>
-            Name
+          <div>
+            <p>Name</p>
             <input
                 name="name"
                 type="text"
                 value={this.state.name} onChange={this.handleChange} />
-          </label>
-          <label>
-            Nickname:
+          </div>
+          <div>
+            <p>Nickname:</p>
               <input
                 name="nickname"
                 type="text"
                 value={this.state.nickname}
                 onChange={this.handleChange} />
-          </label>
-          <label>
-            Description:
+          </div>
+          <div>
+            <p>Description:</p>
               <input
                 name="description"
                 type="text"
                 value={this.state.description}
                 onChange={this.handleChange} />
-          </label>
-          <label>
-            Directions:
+          </div>
+          <div>
+            <p>Directions:</p>
               <input
                 name="directions"
                 type="text"
                 value={this.state.directions}
                 onChange={this.handleChange} />
-          </label>
-          <select name="bathymetry" value={this.state.bathymetry} onChange={this.handleChange} >
-              <option value="sand">Sand</option>
-              <option value="reef">Reef</option>
-              <option value="sand-reef">Sand And Reef</option>
-          </select>
-          <select name="wavetype" value={this.state.wavetype} onChange={this.handleChange} >
-              <option value="beachbreak">Beach Break</option>
-              <option value="reefbreak">Reef Break</option>
-              <option value="pointbreak">Point Break</option>
-          </select>
-          <input type="range" min="1" max="100" value={this.state.wavelength} className="value-slider" id="length-slider" onChange={this.handleChange} />
+          </div>
+          <div>
+            <select name="bathymetry" value={this.state.bathymetry} onChange={this.handleChange} >
+                <option value="sand">Sand</option>
+                <option value="reef">Reef</option>
+                <option value="sand-reef">Sand And Reef</option>
+            </select>
+          </div>
+          <div>
+            <select name="wavetype" value={this.state.wavetype} onChange={this.handleChange} >
+                <option value="beachbreak">Beach Break</option>
+                <option value="reefbreak">Reef Break</option>
+                <option value="pointbreak">Point Break</option>
+            </select>
+          </div>
+          <div>
+            <p>Wave Length:</p>
+            <input name="wavelength" type="range" min="1" max="100" value={this.state.wavelength} className="value-slider" id="length-slider" onChange={this.handleChange} />
+          </div>
+          <div>
+            <p>Wave Quality:</p>
+            <input name="wavequality" type="range" min="1" max="100" value={this.state.wavequality} className="value-slider" id="quality-slider" onChange={this.handleChange} />
+          </div>
+          <div>
+            <select name="wavedirection" value={this.state.direction} onChange={this.handleChange} >
+                <option value="right">Right</option>
+                <option value="left">Left</option>
+                <option value="rightleft">Right And Left</option>
+            </select>
+          </div>
+          <div>
+            <p>Wave Hollowness:</p>
+            <input name="wavehollowness" type="range" min="1" max="100" value={this.state.wavehollowness} className="value-slider" id="hollowness-slider" onChange={this.handleChange} />
+          </div>
+          <div>
+            <p>Wave Danger:</p>
+            <input name="wavedanger" type="range" min="1" max="100" value={this.state.wavedanger} className="value-slider" id="danger-slider" onChange={this.handleChange} />
+          </div>
           <button onClick={this.handleSubmit} />
         </form>
       </div>
@@ -88,8 +118,8 @@ class AddSpot extends Component {
   }
 }
 const MUTATION = gql`
-mutation AddSpot($name: String, $nickname: String, $description: String, $directions: String, $bathymetry: String, $wavetype: String, $wavelength: Int) {
-  insert_Waves(objects: {name: $name, nickname: $nickname, description: $description, directions: $directions, bathymetry: $bathymetry, wavetype: $wavetype, wavelength: $wavelength}) {
+mutation AddSpot($name: String, $nickname: String, $description: String, $directions: String, $bathymetry: String, $wavetype: String, $wavelength: Int, $wavequality: Int, $wavedirection: String, $wavedanger: Int, $wavehollowness: Int) {
+  insert_Waves(objects: {name: $name, nickname: $nickname, description: $description, directions: $directions, bathymetry: $bathymetry, wavetype: $wavetype, wavelength: $wavelength, wavequality: $wavequality, wavedirection: $wavedirection, wavehollowness: $wavehollowness, wavedanger: $wavedanger}) {
     returning {
       name,
       nickname,
@@ -97,7 +127,11 @@ mutation AddSpot($name: String, $nickname: String, $description: String, $direct
       directions,
       bathymetry,
       wavetype,
-      wavelength
+      wavelength,
+      wavequality,
+      wavedirection,
+      wavehollowness,
+      wavedanger
     }
   }
 }
