8f39f62bc6189bf8c6b845086bd94abed83bd176
luke <lukehtravis@gmail.com>
2019-06-16 14:56:33 -0700

    feat: routing for new components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import {BrowserRouter as Router, Route} from "react-router-dom";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
 import AddSpot from "./Components/AddSpot";
+import Search from "./Components/Search";
+import Login from "./Components/Login";
 
 class App extends Component {
   render() {
@@ -12,6 +14,8 @@ class App extends Component {
           <Header />
           <Route exact path="/" component={Home} />
           <Route path="/AddSpot" component={AddSpot} />
+          <Route path="/Search" component={Search} />
+          <Route path="/Login" component={Login} />
         </Fragment>
       </Router>
     );
