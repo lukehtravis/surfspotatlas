8c473cd6f9bc04cdf817c7f89625ed8aa79c1a6e
luke <lukehtravis@gmail.com>
2019-06-16 14:57:02 -0700

    comsetic: change look of header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,5 @@
 import React, {Component} from "react";
 import Grid from '@material-ui/core/Grid';
-import AppBar from '@material-ui/core/AppBar';
 import { Link, Route } from 'react-router-dom';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
@@ -8,13 +7,13 @@ import AddIcon from '@material-ui/icons/Add';
 class Header extends Component {
   render() {
     return (
-      <AppBar>
+      <div>
         <p>Surf Spot Atlas</p>
         <p><Link to="/AddSpot">Add Spot</Link></p>
         <Fab>
           <AddIcon />
         </Fab>
-      </AppBar>    
+      </div>
     )
   }
 }
