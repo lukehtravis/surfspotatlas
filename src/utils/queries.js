779d883f82e4c4b85908cbf6bd0aa356c0f93084
luke <lukehtravis@gmail.com>
2019-09-19 11:13:13 -0700

    feat: get areas code modified to sort results

--- src/utils/queries.js
+++ src/utils/queries.js
@@ -14,4 +14,4 @@ export const FETCH_WAVE_QUALITY = "query fetchWaveQuality($id: bigint) { Wave_Ra
 export const FETCH_WAVE_LENGTH = "query FetchWaveLength($id: Int) { Wave_Ratings_aggregate(where: {id: {_eq: $id}}) { aggregate { avg { wavelength } } } }"
 export const FETCH_WIND_ANGLES = "query FetchWindAngles($id: Int) { Wave_Ratings_aggregate(where: {waveid: {_eq: $id}}) { aggregate { avg { windangleone windangletwo } } } }"
 export const FETCH_TIDES = "query FetchTides($id: bigint) { Wave_Ratings_aggregate(where: {waveid: {_eq: $id}}) { aggregate { avg { lowtide, hightide } } } }"
-export const FETCH_CHOSEN_AREAS = "query FetchChosenAreas($listOfAreas: [Int]) { Locations(where: {id: {_in: $listOfAreas}}) { id longitude latitude area region country continent } }"
+export const FETCH_CHOSEN_AREAS = "query FetchChosenAreas($listOfAreas: [String]) { Locations(where: {area: {_in: $listOfAreas}}, order_by: {latitude: desc, area: asc}) { id longitude latitude area region country continent } }"
