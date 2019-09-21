d630fe7bd865c2419c4a01992c7a5644a738a96d
luke <lukehtravis@gmail.com>
2019-09-21 14:30:29 -0700

    feat: location filters are working for the search feature

--- src/Components/SearchFilters.js
+++ src/Components/SearchFilters.js
@@ -10,46 +10,83 @@ class SearchFilters extends Component {
   }
 
   handleChange = (event) => {
+    console.log("event", event);
+    console.log("event target", event.target)
     const target = event.target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
     const name = target.name;
-
-    this.setState({
-      [name]: value
-    });
+    if (value != "select") {
+      this.setState({
+        [name]: value,
+      });
+    } else {
+      this.setState({
+        [name]: undefined
+      })
+    }
   }
 
   render() {
+    if (!this.props.data.continents) {
+      return "Loading..."
+    }
+    // Store props in seperate vars to avoid mutating state (props) directly
+    let countries = this.props.data.countries;
+    let regions = this.props.data.regions;
+    let areas = this.props.data.areas;
+    console.log("countries", countries, "regions", regions, "area", areas, "reactState = ", this.state)
+    if (this.state.continent) {
+      countries = countries.filter((country) => country.continent == this.state.continent)
+    } else {
+      countries = []
+    }
+    if (this.state.continent && this.state.country) {
+      regions = regions.filter((region) => region.country == this.state.country)
+    } else {
+      regions = []
+    }
+    if (this.state.continent && this.state.country && this.state.region) {
+      areas = areas.filter((area) => area.region == this.state.region)
+    } else {
+      areas = []
+    }
+    console.log("countries", countries, "regions", regions, "area", areas, "reactState = ", this.state)
     return (
       <form>
-        <div>
-          <select name="continent" value={this.state.continent} onChange={this.handleChange} >
-              <option value="sand">Sand</option>
-              <option value="reef">Reef</option>
-              <option value="sand-reef">Sand And Reef</option>
-          </select>
-        </div>
-        <div>
-          <select name="country" value={this.state.country} onChange={this.handleChange} >
-              <option value="beachbreak">Beach Break</option>
-              <option value="reefbreak">Reef Break</option>
-              <option value="pointbreak">Point Break</option>
-          </select>
-        </div>
-        <div>
-          <select name="region" value={this.state.region} onChange={this.handleChange} >
-              <option value="pier">Pier</option>
-              <option value="jetty">Jetty</option>
-              <option value="channel">Channel</option>
-          </select>
-        </div>
-        <div>
-          <select name="area" value={this.state.area} onChange={this.handleChange} >
-              <option value="pier">Pier</option>
-              <option value="jetty">Jetty</option>
-              <option value="channel">Channel</option>
-          </select>
-        </div>
+      <div>
+        <select name="continent" onChange={this.handleChange} >
+          <option value="select">Select</option>
+          {this.props.data.continents.map((continent) => {
+            return <option onClick={this.handleChange} value={continent.continent}>{continent.continent}</option>
+          })}
+        </select>
+      </div>
+      <div>
+        <select name="country" onChange={this.handleChange} >
+          <option value="select">Select</option>
+          {
+            countries.map((country) => {
+              return <option value={country.country}>{country.country}</option>
+            })
+          }
+        </select>
+      </div>
+      <div>
+        <select name="region" onChange={this.handleChange} >
+          <option value="select">Select</option>
+          {regions.map((region) => {
+            return <option value={region.region}>{region.region}</option>
+          })}
+        </select>
+      </div>
+      <div>
+        <select name="area" onChange={this.handleChange} >
+          <option value="select">Select</option>
+          {areas.map((area) => {
+            return <option value={area.area}>{area.area}</option>
+          })}
+        </select>
+      </div>
       </form>
     )
   }
