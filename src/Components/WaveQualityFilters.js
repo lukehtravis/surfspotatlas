3f60cb56a1d2e98185bda4c07384fcf4befeefac
luke <lukehtravis@gmail.com>
2019-09-21 17:48:34 -0700

    feat: create wave quality filters component

---
+++ src/Components/WaveQualityFilters.js
@@ -0,0 +1,15 @@
+import React, {Compnent} from "react";
+import {graphql} from "react-apollo";
+import gql from "graphql-tag";
+
+const WaveQualityFilters = (props) => {
+
+
+  return(
+    <div>
+      <p>Wave Quality Filters</p>
+    </div>
+  )
+}
+
+export default WaveQualityFilters
