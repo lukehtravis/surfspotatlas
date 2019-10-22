78e8fabeaf9c7a6bf5ebebd735dad8695aaabc9e
luke <lukehtravis@gmail.com>
2019-10-22 13:21:11 -0700

    feat: sorting of area category implemented on tables

--- src/Components/SearchedSpotsTable.js
+++ src/Components/SearchedSpotsTable.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import {Link} from "react-router-dom";
-import {withStyles} from "@material-ui/core/styles"
+import {withStyles} from "@material-ui/core/styles";
+import { lighten, makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -8,9 +9,19 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import StaticProgressBar from "./StaticProgressBar";
+import TableSortLabel from '@material-ui/core/TableSortLabel';
 import {bathymetryStringConverter, directionStringConverter} from "../utils/dbNameConversions";
 
-const styles = theme => ({
+/*
+
+  This component takes in spots array of objects from SurfSpotsList and creates a table
+  that displays the spots for chosen areas. The table is sortable.
+
+  Implementation derived from examples here
+  https://material-ui.com/components/tables/
+
+*/
+const useStyles = makeStyles(theme => ({
   root: {
     width: '100%',
     overflowX: 'auto',
@@ -18,51 +29,120 @@ const styles = theme => ({
   table: {
     minWidth: 200,
   },
-});
+}));
 
-class SearchedSpotsTable extends Component {
-  constructor(props) {
-    super(props);
-
+function desc(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
   }
-
-
-  render(){
-    const {spots, classes} = this.props
-
-    return (
-      <div>
-        <Paper className={classes.root}>
-          <Table className={classes.table} aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell>Name</TableCell>
-                <TableCell align="left">Area</TableCell>
-                <TableCell align="left">Direction</TableCell>
-                <TableCell align="left">Bathymetry</TableCell>
-                <TableCell align="center">Quality</TableCell>
-                <TableCell align="center">Danger</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {spots.map(spot => (
-                <TableRow key={spot.name}>
-                  <TableCell component="th" component={Link} to={`Wave/${spot.id}`} scope="row">
-                    {spot.name}
-                  </TableCell>
-                  <TableCell align="left">{spot.area}</TableCell>
-                  <TableCell align="left">{directionStringConverter(spot.direction)}</TableCell>
-                  <TableCell align="left">{bathymetryStringConverter(spot.bathymetry)}</TableCell>
-                  <TableCell align="right"><StaticProgressBar value={Math.round(spot.quality)} /></TableCell>
-                  <TableCell align="right"><StaticProgressBar value={Math.round(spot.danger)} /></TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </Paper>
-      </div>
-    )
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
   }
+  return 0;
 }
 
-export default withStyles(styles)(SearchedSpotsTable);
+function stableSort(array, cmp) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = cmp(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map(el => el[0]);
+}
+
+function getSorting(order, orderBy) {
+  return order === 'desc' ? (a, b) => desc(a, b, orderBy) : (a, b) => -desc(a, b, orderBy);
+}
+
+const headCells = [
+  { id: 'name', numeric: false, disablePadding: false, label: 'Name' },
+  { id: 'area', numeric: false, disablePadding: false, label: 'Area' },
+  { id: 'direction', numeric: false, disablePadding: false, label: 'Direction' },
+  { id: 'bathymetry', numeric: false, disablePadding: false, label: 'Bottom' },
+  { id: 'waveQuality', numeric: true, disablePadding: false, label: 'Quality' },
+  { id: 'waveDanger', numeric: true, disablePadding: false, label: 'Danger' }
+];
+
+function EnhancedTableHead(props) {
+  const { classes, onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } = props;
+  const createSortHandler = property => event => {
+    onRequestSort(event, property);
+  };
+
+  return (
+    <TableHead>
+      <TableRow>
+        {headCells.map(headCell => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? 'right' : 'left'}
+            padding={headCell.disablePadding ? 'none' : 'default'}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={order}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <span className={classes.visuallyHidden}>
+                  {order === 'desc' ? '' : ''}
+                </span>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+export default function SearchedSpotsTable(props) {
+  const classes = useStyles();
+  const {spots} = props;
+  const [order, setOrder] = React.useState('asc');
+  const [orderBy, setOrderBy] = React.useState('area');
+
+  const handleRequestSort = (event, property) => {
+    const isDesc = orderBy === property && order === 'desc';
+    setOrder(isDesc ? 'asc' : 'desc');
+    setOrderBy(property);
+  };
+
+  return (
+    <div>
+      <Paper className={classes.root}>
+        <Table className={classes.table} aria-label="simple table">
+          <EnhancedTableHead
+            order={order}
+            orderBy={orderBy}
+            onRequestSort={handleRequestSort}
+            classes={classes}
+          />
+          <TableBody>
+            {stableSort(spots, getSorting(order, orderBy))
+              //.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((spot, index) => {
+                const labelId = `enhanced-table-checkbox-${index}`;
+                return (
+                  <TableRow key={spot.name} hover>
+                    <TableCell component="th" id={labelId} component={Link} to={`Wave/${spot.id}`} scope="row">
+                      {spot.name}
+                    </TableCell>
+                    <TableCell align="left" component={Link} to={`Area/${spot.area}`}>{spot.area}</TableCell>
+                    <TableCell align="left">{directionStringConverter(spot.direction)}</TableCell>
+                    <TableCell align="left">{bathymetryStringConverter(spot.bathymetry)}</TableCell>
+                    <TableCell align="right"><StaticProgressBar value={Math.round(spot.quality)} /></TableCell>
+                    <TableCell align="right"><StaticProgressBar value={Math.round(spot.danger)} /></TableCell>
+                  </TableRow>
+                )
+              })
+            }
+          </TableBody>
+        </Table>
+      </Paper>
+    </div>
+  )
+}
