8564bef6511a196db378ef2413c4fb83cfc6caea
luke <lukehtravis@gmail.com>
2019-10-24 00:59:07 -0700

    design/feat: desing of tide range slider improved

--- src/Components/TideSlider.js
+++ src/Components/TideSlider.js
@@ -3,19 +3,64 @@ import {graphql} from "react-apollo";
 import gql from "graphql-tag";
 import Slider from '@material-ui/core/Slider';
 import {FETCH_TIDES} from '../utils/queries';
+import {withStyles} from "@material-ui/core/styles";
+import {tideMarks} from "../utils/labels";
+import Typography from "@material-ui/core/Typography";
+
+/*
+  This component represents sliders with two inputs, which do not allow user
+  interaction.
+  The sliders take in two values, each of which represents one "thumb" or circle,
+  which represents the extremes of a range (a range that is the average of user)
+  contributed attribute rankings in a database.
+  The bar height is set to default breakpoint by breakpoint, and then elements are
+  adjusted around that (numbers below were fidgeted with to meet design spec)
+
+  TODO... MuiSlider-markLabel & MuiSlider-mark are classes that control the marks (words below the bar)
+  The docs don't say anything about how to style them via component, so currently that styling
+  lives in App.css
+*/
+
+
+const barHeight = 15
+
+const styles = theme => {
+  return {
+    root: {
+      height: theme.breakpoints.width
+    },
+    track: {
+      height: barHeight,
+      borderRadius: barHeight * 0.5,
+    },
+    thumb: {
+      height: (barHeight * 1.66),
+      width: (barHeight * 1.66),
+      marginTop: barHeight * -0.33,
+      opacity: 0.6,
+      marginLeft: (barHeight * 1.66) * -0.5
+    },
+    rail: {
+      height: barHeight
+    },
+  }
+}
 
 const TideSlider = (props) => {
   if (props.data.loading) {
     return "Loading..."
   }
-
+  const classes = props.classes;
   const lowtide = props.data.Wave_Ratings_aggregate.aggregate.avg.lowtide
   const hightide = props.data.Wave_Ratings_aggregate.aggregate.avg.hightide
   return (
-    <Slider value={[lowtide, hightide]} valueLabelDisplay="on" />
+    <div>
+      <Typography>Ideal Tide Range</Typography>
+      <Slider marks={tideMarks} value={[lowtide, hightide]} classes={{...classes}} />
+    </div>
   )
 }
 
 export default graphql(gql`${FETCH_TIDES}`, {
   options: (props) => {return {variables: {id: props.waveId}}}
-})(TideSlider)
+})(withStyles(styles)(TideSlider))
