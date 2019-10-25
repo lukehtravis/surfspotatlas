fe5a7b70a1eb9f710db805611312eedd416127ca
luke <lukehtravis@gmail.com>
2019-10-25 15:48:43 -0700

    feat: working image gallery masonry grid

--- src/Components/WaveImageGallery.js
+++ src/Components/WaveImageGallery.js
@@ -1,8 +1,25 @@
 import React, {Component} from "react";
 import {graphql} from "react-apollo";
 import gql from "graphql-tag";
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
 import {FIREBASE_BUCKET} from "../utils/constants";
 import {FETCH_WAVE_IMAGES} from "../utils/queries"
+import {withStyles} from "@material-ui/core/styles";
+
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    width: 500,
+    maxHeight: 450,
+  }
+})
 
 /*
   User-submitted images are stored in a google firebase storage db. References
@@ -24,15 +41,16 @@ class WaveImageGallery extends Component {
     this.props.data.Wave_Images.map(image => {
       images.push({url: image.url, id:image.id})
     })
-    
+    const {classes} = this.props
     return(
-      <div>
-        <p>Wave Image Gallery</p>
-        <div>
-          {images.map(image => {
-            return <img src={image.url} />
-          })}
-        </div>
+      <div className={classes.root}>
+        <GridList cellHeight={160} className={classes.gridList} cols={2}>
+          {images.map(tile => (
+            <GridListTile key={tile.url} cols={tile.cols || 1}>
+              <img src={tile.url} />
+            </GridListTile>
+          ))}
+        </GridList>
       </div>
     )
   }
@@ -40,4 +58,4 @@ class WaveImageGallery extends Component {
 
 export default graphql(gql`${FETCH_WAVE_IMAGES}`, {
   options: (props) => {return {variables: {waveid: props.waveId}}}
-})(WaveImageGallery);
+})(withStyles(styles)(WaveImageGallery));
