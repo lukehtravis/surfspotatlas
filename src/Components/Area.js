76d64541e1a23787d1c22dea997ae088048b117f
luke <lukehtravis@gmail.com>
2019-10-31 15:36:06 -0700

    feat:add table to Area component

--- src/Components/Area.js
+++ src/Components/Area.js
@@ -5,6 +5,7 @@ import { graphql } from 'react-apollo';
 import gql from "graphql-tag";
 import Pin from "./Pin";
 import AreaMap from "./AreaMap";
+import AreaTable from "./AreaTable";
 import {FETCH_AREA_LOCATION_DATA} from "../utils/queries";
 
 const MAP_STYLE = 'mapbox://styles/mapbox/streets-v11';
@@ -19,11 +20,34 @@ class Area extends Component {
     if (!this.props.data.Locations) {
       return "Loading"
     }
-
+    let mapLocations = this.props.data.Locations.map(location => {
+      return {id: location.id, latitude: location.latitude, longitude: location.longitude}
+    })
+    let tableLocations = this.props.data.Locations.map(location => {
+      return {
+        id: location.Wave.id,
+        name: location.Wave.name,
+        direction: location.Wave.wavedirection,
+        bathymetry: location.Wave.bathymetry,
+        quality: location.Wave.Wave_Ratings_aggregate.aggregate.avg.wavequality,
+        danger: location.Wave.Wave_Ratings_aggregate.aggregate.avg.wavequality,
+        area: this.props.match.params.areaName
+      }
+    })
+    /*
+    id: spot.Wave.id,
+    name: spot.Wave.name,
+    direction: spot.Wave.wavedirection,
+    bathymetry: spot.Wave.bathymetry,
+    quality: spot.Wave.Wave_Ratings_aggregate.aggregate.avg.wavequality,
+    danger: spot.Wave.Wave_Ratings_aggregate.aggregate.avg.wavedanger,
+    area: spot.area
+    */
     return (
       <div>
         <p>Area</p>
-        <AreaMap areaSpots={this.props.data.Locations} />
+        <AreaMap areaSpots={mapLocations} />
+        <AreaTable spots={tableLocations} />
       </div>
     )
   }
@@ -32,3 +56,49 @@ class Area extends Component {
 export default graphql(gql`${FETCH_AREA_LOCATION_DATA}`, {
   options: (props) => {return {variables: {areaName: props.match.params.areaName} } }
 })(Area)
+/*
+{
+  "data": {
+    "Locations": [
+      {
+        "latitude": 32.9975969236175,
+        "longitude": -117.27655321564285,
+        "id": 36,
+        "Wave": {
+          "name": "tabletops",
+          "Wave_Ratings_aggregate": {
+            "aggregate": {
+              "avg": {
+                "wavequality": 80,
+                "wavedanger": 54
+              }
+            }
+          },
+          "bathymetry": "reef",
+          "wavedirection": "left",
+          "id": 40
+        }
+      },
+      {
+        "latitude": 33.03498381683269,
+        "longitude": -117.29280788193414,
+        "id": 35,
+        "Wave": {
+          "name": "Swamis",
+          "Wave_Ratings_aggregate": {
+            "aggregate": {
+              "avg": {
+                "wavequality": 64.66666666666667,
+                "wavedanger": 74
+              }
+            }
+          },
+          "bathymetry": "sand-reef",
+          "wavedirection": null,
+          "id": 39
+        }
+      }
+    ]
+  }
+}
+*/
---
+++ src/Components/AreaTable.js
@@ -0,0 +1,104 @@
+import React, {Component} from "react";
+import {Link} from "react-router-dom";
+import {withStyles} from "@material-ui/core/styles";
+import { lighten, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import Paper from '@material-ui/core/Paper';
+import StaticProgressBar from "./StaticProgressBar";
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import EnhancedTableHeader from "./EnhancedTableHeader";
+import {bathymetryStringConverter, directionStringConverter} from "../utils/dbNameConversions";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    overflowX: 'auto',
+    backgroundColor: theme.palette.background.default
+  },
+  table: {
+    minWidth: 200,
+    width: "90%",
+    margin: `${theme.spacing(4)}px auto`,
+    backgroundColor: theme.palette.background.paper,
+    borderRadius: "5px"
+  },
+  tableCell: {
+    border: "none"
+  }
+}));
+
+function desc(a, b, orderBy) {
+  console.log("desc", a,b,orderBy)
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function stableSort(array, cmp) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    console.log("stableSort", a,b)
+    const order = cmp(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map(el => el[0]);
+}
+
+function getSorting(order, orderBy) {
+  return order === 'desc' ? (a, b) => desc(a, b, orderBy) : (a, b) => -desc(a, b, orderBy);
+}
+
+export default function SearchedSpotsTable(props) {
+  const classes = useStyles();
+  const {spots} = props;
+  const [order, setOrder] = React.useState('asc');
+  const [orderBy, setOrderBy] = React.useState('area');
+
+  const handleRequestSort = (event, property) => {
+    const isDesc = orderBy === property && order === 'desc';
+    setOrder(isDesc ? 'asc' : 'desc');
+    setOrderBy(property);
+  };
+  return (
+    <div>
+      <Paper className={classes.root}>
+        <Table className={classes.table} aria-label="simple table">
+          <EnhancedTableHeader
+            order={order}
+            orderBy={orderBy}
+            onRequestSort={handleRequestSort}
+            classes={classes}
+          />
+          <TableBody>
+            {stableSort(spots, getSorting(order, orderBy))
+              //.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((spot, index) => {
+                const labelId = `enhanced-table-checkbox-${index}`;
+                return (
+                  <TableRow key={spot.name} hover>
+                    <TableCell className={classes.tableCell} component="th" id={labelId} component={Link} to={`Wave/${spot.id}`} scope="row">
+                      {spot.name}
+                    </TableCell>
+                    <TableCell className={classes.tableCell} align="left" component={Link} to={`Area/${spot.area}`}>{spot.area}</TableCell>
+                    <TableCell className={classes.tableCell} align="left">{directionStringConverter(spot.direction)}</TableCell>
+                    <TableCell className={classes.tableCell} align="left">{bathymetryStringConverter(spot.bathymetry)}</TableCell>
+                    <TableCell className={classes.tableCell} align="center"><StaticProgressBar value={Math.round(spot.quality)} /></TableCell>
+                    <TableCell className={classes.tableCell} align="center"><StaticProgressBar value={Math.round(spot.danger)} /></TableCell>
+                  </TableRow>
+                )
+              })
+            }
+          </TableBody>
+        </Table>
+      </Paper>
+    </div>
+  )
+}
