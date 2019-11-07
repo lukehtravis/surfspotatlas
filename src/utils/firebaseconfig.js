d28d2804f0f426dace96d9aade7e9c8975238f05
luke <lukehtravis@gmail.com>
2019-11-07 14:44:31 -0800

    feat: ;)

---
+++ src/utils/firebaseconfig.js
@@ -0,0 +1,11 @@
+const fbcfg = {
+  apiKey: "AIzaSyAIwOaeqXqiJNqm_C5yvO_cWmm48fBiQNU",
+  authDomain: "surfspotatlas.firebaseapp.com",
+  databaseURL: "https://surfspotatlas.firebaseio.com",
+  projectId: "surfspotatlas",
+  storageBucket: "surfspotatlas.appspot.com",
+  messagingSenderId: "348127454502",
+  appId: "1:348127454502:web:4870568f0f2bab6da2cde7",
+}
+
+export default fbcfg;
